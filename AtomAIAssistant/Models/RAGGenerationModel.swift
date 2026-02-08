28e004c1720e90db3a86f4fed28937e19de5b40b
gowthamnamuru <namuru.gowtham@aa.com>
2026-02-08 22:36:17 +0530

    Added RAG Generation Model

---
+++ AtomAIAssistant/Models/RAGGenerationModel.swift
@@ -0,0 +1,35 @@
+//
+//  RAGGenerationModel.swift
+//  AtomAIAssistant
+//
+//  Created by Gowtham, Namuru on 08/02/26.
+//
+
+import Foundation
+import SwiftData
+
+enum AvailableMarkdown: String, CaseIterable {
+    case ipad_accessories
+
+    var url: URL {
+        Bundle.main.url(forResource: self.rawValue, withExtension: "md")!
+    }
+}
+
+final class RAGGenerationModel {
+    private var modelContext: ModelContext?
+    private var multiPDFEmbeddingPipeline: MultiPDFEmbeddingPipeLine?
+    private let embeddingClient: EmbeddingProvider = AppleEmbeddingClient()
+    private var pdfURLs: [AvailableMarkdown]
+
+    init(pdfs: [AvailableMarkdown], multiPDFEmbeddingPipeline: MultiPDFEmbeddingPipeLine? = nil) {
+        self.pdfURLs = pdfs
+        self.multiPDFEmbeddingPipeline = multiPDFEmbeddingPipeline
+    }
+
+    @MainActor
+    func generateEmbeddings(progress: @escaping (DetailedEmbeddingProgress) -> Void) async throws {
+        let urls = AvailableMarkdown.allCases.map { $0.url }
+        try await multiPDFEmbeddingPipeline?.processAllPDFs(pdfURLs: urls, onProgress: progress)
+    }
+}
